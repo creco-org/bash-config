ec8884241a8106b8f086aa77418560f56c9bbe78
CreatiCoding <seokho.jeong@toss.im>
2021-11-04 23:58:04 +0900

    fix

--- bash-config.sh
+++ bash-config.sh
@@ -34,19 +34,6 @@ function current_repository() {
 alias gitpull='git pull origin $(current_branch)'
 
 # zsh alias
-alias ...=../..
-alias ....=../../..
-alias .....=../../../..
-alias ......=../../../../..
-alias 1='cd -'
-alias 2='cd -2'
-alias 3='cd -3'
-alias 4='cd -4'
-alias 5='cd -5'
-alias 6='cd -6'
-alias 7='cd -7'
-alias 8='cd -8'
-alias 9='cd -9'
 alias grep='grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox}'
 alias l='ls -lah'
 alias la='ls -lAh'
@@ -63,6 +50,9 @@ alias wrun='wrun(){ wget -q -O - $1 | bash;}; wrun'
 alias rd=rmdir
 alias run-help=man
 alias which-command=whence
+alias docker=nerdctl
+alias gwc='git diff HEAD --shortstat'
+alias grep="rg"
 
 # https://stackoverflow.com/questions/9457233/unlimited-bash-history#answer-19533853
 # history size unlimit
