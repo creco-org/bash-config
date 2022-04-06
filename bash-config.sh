2591942356557db39ac7fdeea3db5a6f4c11bd32
정석호 <creaticoding@gmail.com>
2022-04-06 15:21:41 +0900

    Update bash-config.sh

--- bash-config.sh
+++ bash-config.sh
@@ -82,7 +82,6 @@ export LSCOLORS=ExFxCxDxBxegedabagacad
 
 eval "$(direnv hook bash)" 2>/dev/null
 
-
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/creco/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
+# echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/creco/.zprofile
+# eval "$(/opt/homebrew/bin/brew shellenv)"
 
