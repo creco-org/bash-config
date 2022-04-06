1180cc89f11eefc9489dc6ef19eec3b6b1649437
정석호 <creaticoding@gmail.com>
2022-04-06 15:49:32 +0900

    Update bash-config.sh

--- bash-config.sh
+++ bash-config.sh
@@ -84,3 +84,8 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 eval "$(direnv hook bash)" 2>/dev/null
 
+# NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
